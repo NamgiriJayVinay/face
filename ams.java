e11272bb75972f15d51e997ef32d694e3bc1fdd6
Jay Vinay <namgirijayvinay@gmail.com>
2025-04-14 11:06:57 +0530

    Update ams.java

--- ams.java
+++ ams.java
@@ -1,1 +1,15 @@
 
+@Dao
+public interface AiMinuteDataDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(AiMinuteDataEntity aiMinuteSample);
+
+    @Query("SELECT access_minute FROM " + TABLE_AI_MINUTE_DATA + " ORDER BY access_minute DESC LIMIT 1")
+    long getLastPreProcessAccessMinute();
+
+    @Query("SELECT * FROM ai_minute_data WHERE access_minute > :lastInfTime ")
+    List<AiMinuteDataEntity> getMinuteDataFromLastInferenceTimestamp(long lastInfTime);
+
+    @Query("DELETE FROM " + TABLE_AI_MINUTE_DATA)
+    void deleteAll();
+}
