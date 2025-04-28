33cbb8756394ebdaccc7178959bedc5c5c348e35
Jay Vinay <namgirijayvinay@gmail.com>
2025-04-28 12:43:36 +0530

    Update ams.java

--- ams.java
+++ ams.java
@@ -1,1 +1,230 @@
+give for this test cases for 5: 15 as pos:neg tcs along with coments of having each case as positive or negative with tc number : 
+public class ModelInferenceManagerImpl implements ModelInferenceManager {
 
+    private final String TAG = AiPrivacyConstants.TAG + ModelInferenceManagerImpl.class.getSimpleName();
+    private final ModelManager modelManager;
+    HashMap<String, String> sensorCountMap = new HashMap<String, String>();
+    HashMap<String, Pair<String, String>> featurePermissionTagMap = new HashMap<String, Pair<String, String>>();
+    SuggestionScoreUtil suggestionScoreUtil = new SuggestionScoreUtil();
+    SuggestionDao suggestionDao;
+    AiPrivacyRepository aiPrivacyRepository;
+    ExecutorService executorService;
+    Future<Void> result = null;
+
+    public ModelInferenceManagerImpl(Context context) {
+        modelManager = ModelManager.getInstance(context);
+        boolean loadModelSuccessFlag = modelManager.loadModel();
+        featurePermissionTagMap = AIModelAttributes.getFeaturePermissionTypeMap();
+        suggestionDao = AppDatabase.getInstance(context).suggestionDao();
+        aiPrivacyRepository = new AiPrivacyRepository(context);
+        executorService = Executors.newSingleThreadExecutor();
+        if (!loadModelSuccessFlag) {
+            Log.w(TAG, "Model Load Failed...");
+        }
+    }
+
+    @Override
+    public int performInference(ArrayList<MinuteWiseInferenceDataPoint> inferenceDataPoints) {
+        if (modelManager == null) {
+            Log.e(TAG, "Model manager is NULL Inference stopped");
+            return AiErrorCode.MODEL_LOAD_ERROR.getValue();
+        }
+        synchronized (this) {
+            try {
+                Log.i(TAG, "Model inference started");
+                long inferenceStartTime = System.currentTimeMillis();
+                int inputRecordSize = inferenceDataPoints.size();
+                for (int i = 0; i < inputRecordSize; i++) {
+                    this.sensorCountMap.clear();
+                    fillSensorCountMap(inferenceDataPoints.get(i), sensorCountMap);
+                    Log.i(TAG, "Sensor count map :: " + sensorCountMap);
+                    Triple<List<ByteBuffer>, Integer, Integer> preProcessedInput = this.modelManager.prepareInputBatchesForInference(1, sensorCountMap);
+                    Map<String, Pair<Float, Float>> anomaliesMap = this.modelManager.runInference(
+                            preProcessedInput.getFirst().get(0),
+                            preProcessedInput.getSecond(),
+                            preProcessedInput.getThird(),
+                            sensorCountMap
+                    );
+                    Log.i(TAG, "Anomalies found in feature :: " + anomaliesMap.keySet());
+                    if (!anomaliesMap.isEmpty()) { // anomaly found
+                        Log.i(TAG, "Anomalies Found ......");
+                        String packageName = inferenceDataPoints.get(i).minuteWisePreProcessEntityForVAE.packageName;
+                        int appCategory = inferenceDataPoints.get(i).minuteWisePreProcessEntityForVAE.appCategory;
+                        int appTrustLevel = inferenceDataPoints.get(i).minuteWisePreProcessEntityForVAE.appTrustLevel;
+                        long timestamp = inferenceDataPoints.get(i).minuteWisePreProcessEntityForVAE.timestamp;
+                        int isForeground = inferenceDataPoints.get(i).minuteWisePreProcessEntityForVAE.isForeground;
+                        int accessHours = (int) inferenceDataPoints.get(i).minuteWisePreProcessEntityForVAE.accessHour;
+                        Log.i(TAG, "Insert in the suggestion entity");
+                        result = (Future<Void>) executorService.submit(() -> {
+                            HashMap<String, String> permissionUsageMap = new HashMap<>(sensorCountMap);
+                            insertOrUpdateSuggestion(packageName, timestamp, appCategory, appTrustLevel, accessHours, anomaliesMap, isForeground, permissionUsageMap);
+                        });
+                        result.get();
+                    } else {
+                        Log.i(TAG, "No Anomaly Found...");
+                    }
+                }
+                Log.i(TAG, "Vae inference time: " + (System.currentTimeMillis() - inferenceStartTime) + "ms" + " for total input records: " + inputRecordSize);
+            } catch (Exception ex) {
+                Log.e(TAG, "Error while performing inference :: " + ex.getMessage());
+                return AiErrorCode.PREPROCESSING_ERROR.getValue();
+            }
+        }
+        return AiErrorCode.SUCCESS.getValue();
+    }
+
+    private void fillSensorCountMap(MinuteWiseInferenceDataPoint minuteWiseInferenceDataPoint, HashMap<String, String> sensorCountMap) {
+        sensorCountMap.put(AIModelAttributes.GRANTED_PERMISSION_ATTRIBUTE_KEY, minuteWiseInferenceDataPoint.minuteWisePreProcessEntityForVAE.grantedPermissions);
+        sensorCountMap.put(AIModelAttributes.AIModelAttributeNames.BACKGROUND_FEATURE.getValue(), Integer.toString(minuteWiseInferenceDataPoint.minuteWisePreProcessEntityForVAE.isForeground));
+        sensorCountMap.put(AIModelAttributes.AIModelAttributeNames.APP_CATEGORY.getValue(), Integer.toString(minuteWiseInferenceDataPoint.minuteWisePreProcessEntityForVAE.appCategory));
+        sensorCountMap.put(AIModelAttributes.AIModelAttributeNames.APP_TRUST_LEVEL.getValue(), Integer.toString(minuteWiseInferenceDataPoint.minuteWisePreProcessEntityForVAE.appTrustLevel));
+        sensorCountMap.put(AIModelAttributes.AIModelAttributeNames.ACCESS_HOURS.getValue(), Integer.toString(minuteWiseInferenceDataPoint.minuteWisePreProcessEntityForVAE.accessHour));
+
+        for (AIModelAttributes.AIModelAttributeNames permissionFeature : AIModelAttributes.AIModelAttributeNames.values()) {
+            try {
+                int val = minuteWiseInferenceDataPoint.getFeatureValue(permissionFeature);
+                if (val > 0) {
+                    sensorCountMap.put(permissionFeature.getValue(), Integer.toString(val));
+                }
+            } catch (FeatureNotFoundException featureNotFoundException) {
+                Log.i(TAG, "Feature not found error");
+            } catch (Exception ex) {
+                Log.e(TAG, "Error occurred while fetching feature value :: " + ex.getMessage());
+            }
+        }
+    }
+
+    public ArrayList<String> getAnomalyFeatures(Boolean[] anomalies, HashMap<String, String> sensorCountMap) {
+        ArrayList<String> anomalyFeatures = new ArrayList<String>();
+        ArrayList<String> aiNumericalFeatures = AIModelAttributes.getNumericalFeatures();
+        for (int i = 0; i < aiNumericalFeatures.size(); i++) {
+            if (anomalies[i] && sensorCountMap.containsKey(aiNumericalFeatures.get(i))) {
+                anomalyFeatures.add(aiNumericalFeatures.get(i));
+            }
+        }
+        return anomalyFeatures;
+    }
+
+    private void insertOrUpdateSuggestion(String packageName, long timestamp, int appCategory, int appTrustLevel, int accessHours, Map<String, Pair<Float, Float>> anomalyFeatureMap, int isForeground, Map<String, String> permissionUsageCountMap) {
+        List<Event> eventList = getEventListFromPermissionList(packageName, timestamp, anomalyFeatureMap, permissionUsageCountMap, isForeground);
+
+        List<SuggestionEntity> existingSuggestionList = aiPrivacyRepository.getSuggestionByPackageNameAndFeature(packageName, SuggestionConstants.FEATURE_VAE);
+        if (!existingSuggestionList.isEmpty()) {
+            SuggestionEntity suggestionEntity = existingSuggestionList.get(0);
+            List<Event> existingEventList = suggestionEntity.event;
+            List<Event> upatedEventList = getUpdatedEventList(existingEventList, eventList);
+            for (int j = 0; j < upatedEventList.size(); j++) {
+                upatedEventList.get(j).setScore(
+                        suggestionScoreUtil.getEventSuggestionScore(
+                                upatedEventList.get(j),
+                                appCategory,
+                                eventList.size()
+                        )
+                );
+            }
+            suggestionEntity.event = upatedEventList;
+            suggestionEntity.lastTimestamp = timestamp;
+            suggestionEntity.suggestionScore = suggestionScoreUtil.getSuggestionScore(
+                    appTrustLevel,
+                    appCategory,
+                    accessHours,
+                    upatedEventList
+            );
+            aiPrivacyRepository.updateSuggestion(suggestionEntity);
+        } else {
+            AppTrustLevelType appTrustLevelType = AppTrustLevelType.getAppTrustLevelType(appTrustLevel);
+            AppCategoryType appCategoryType = AppCategoryType.getAppCategoryType(appCategory);
+            double suggestionScore = suggestionScoreUtil.getSuggestionScore(
+                    appTrustLevel,
+                    appCategory,
+                    accessHours,
+                    eventList
+            );
+            for (int j = 0; j < eventList.size(); j++) {
+                eventList.get(j).setScore(
+                        suggestionScoreUtil.getEventSuggestionScore(
+                                eventList.get(j),
+                                appCategory,
+                                eventList.size()
+                        )
+                );
+            }
+            SuggestionEntity suggestion = new SuggestionEntity(
+                    timestamp,
+                    packageName,
+                    appTrustLevelType,
+                    appCategoryType,
+                    SuggestionConstants.FEATURE_VAE,
+                    eventList,
+                    suggestionScore
+            );
+            aiPrivacyRepository.insertSuggestions(suggestion);
+        }
+    }
+
+    private ArrayList<Event> getEventListFromPermissionList(String packageName, long timestamp, Map<String, Pair<Float, Float>> anomalyFeatureMap, Map<String, String> sensorCountMap, int isForeground) {
+        ArrayList<Event> eventList = new ArrayList<Event>();
+        for (String anomalyFeat : anomalyFeatureMap.keySet()) {
+            if(AiPrivacyConstants.exemptedPermissions.contains(featurePermissionTagMap.get(anomalyFeat).first)) {
+                Log.i(TAG, "Permission :: " + featurePermissionTagMap.get(anomalyFeat) + " detected and skipped.");
+                continue;
+            }
+            Event event = new Event(
+                    featurePermissionTagMap.get(anomalyFeat).first,
+                    featurePermissionTagMap.get(anomalyFeat).second,
+                    isForeground == 1 ? SuggestionConstants.FOREGROUND_STATE : SuggestionConstants.BACKGROUND_STATE,
+                    Integer.parseInt(sensorCountMap.get(anomalyFeat)),
+                    timestamp,
+                    0,
+                    anomalyFeatureMap.get(anomalyFeat).first,
+                    anomalyFeatureMap.get(anomalyFeat).second
+            );
+            event.setTotalForegroundUsage(
+                    aiPrivacyRepository.getPermissionUsageLast24Hours(
+                            featurePermissionTagMap.get(anomalyFeat).first,
+                            packageName,
+                            timestamp,
+                            AccessStateType.FOREGROUND.getValue()
+
+                    )
+            );
+            event.setTotalBackgroundUsage(
+                    aiPrivacyRepository.getPermissionUsageLast24Hours(
+                            featurePermissionTagMap.get(anomalyFeat).first,
+                            packageName,
+                            timestamp,
+                            AccessStateType.BACKGROUND.getValue()
+                    )
+            );
+            eventList.add(event);
+        }
+        return eventList;
+    }
+
+    private ArrayList<Event> getUpdatedEventList(List<Event> existingEventList, List<Event> newEventList) {
+        ArrayList<Event> updatedEventList = new ArrayList<Event>(newEventList);
+        for (Event existingEvent : existingEventList) {
+            boolean isSameEventPresent = false;
+            for (Event newEvent : newEventList) {
+                if (isSameEvent(newEvent, existingEvent)) {
+                    isSameEventPresent = true;
+                    break;
+                }
+            }
+            if (!isSameEventPresent) {
+                updatedEventList.add(existingEvent);
+            }
+        }
+        return updatedEventList;
+    }
+
+    private boolean isSameEvent(Event a, Event b) {
+        return (Objects.equals(a.getPermission(), b.getPermission())) && (Objects.equals(a.getType(), b.getType())) && (Objects.equals(a.getState(), b.getState()));
+    }
+
+}
+which uses public interface ModelInferenceManager {
+
+    int performInference(ArrayList<MinuteWiseInferenceDataPoint> inferenceDataPoints);
+
+}
